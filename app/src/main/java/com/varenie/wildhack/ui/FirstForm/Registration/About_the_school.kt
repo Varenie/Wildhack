6a35e0c6d96af961213ef55832fdaff3c1d9aa4f
mihai <mihai@LAPTOP>
2021-12-04 17:30:49 +0300

    text_commit

---
+++ app/src/main/java/com/varenie/wildhack/ui/FirstForm/Registration/About_the_school.kt
@@ -0,0 +1,22 @@
+package com.varenie.wildhack.ui.FirstForm.Registration
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.varenie.wildhack.R
+
+class About_the_school : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_the_school, container, false)
+    }
+
+
+}
